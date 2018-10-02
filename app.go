4f314c39e328605925761b5a1cb4b541f10b57c0
xiangdongdong <xiangdongdong@chelun.com>
2018-10-02 13:37:55 +0800

    1.插入算法

--- app.go
+++ app.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"./insertSort"
-	"./quickSort"
 )
 
 func main() {
 	//原始快排
-	Qs.OriginalQuickSort()
+	//Qs.OriginalQuickSort()
 	//优化快排
-	Qs.OptimizationQuickSort()
+	//Qs.OptimizationQuickSort()
 	//插入算法
 	IS.InsertSort()
 }
--- insertSort/insertSort.go
+++ insertSort/insertSort.go
@@ -4,24 +4,50 @@ import "fmt"
 import "../tool"
 
 func InsertSort() {
-	arrList := []int{1, 2, 8, 11, 3, 6, 8, 9, 343, 3}
-	standardInsertSort(arrList)
+	arrList := []int{1, 2, 8, 11, 3, 6, 8, 4, 9, 343, 3}
+	arrList = standardInsertSort(arrList)
 	fmt.Println("InsertSort:")
 	tool.PrintSlice(arrList)
 }
 
-func standardInsertSort(list []int) {
+func standardInsertSort(list []int) []int {
+	resultList := []int{}
 	length := len(list)
-	i := 0
+	i := 1
+	resultList = append(resultList, list[0])
 	for i < length {
-		tmp := list[i]
-		for j := i + 1; j < length; j++ {
-			if tmp <= list[j] {
-				tool.SliceSwap(list, i, j)
-				tool.SliceSwap(list, i+1, j)
-				i++
+		tool.PrintSlice(resultList)
+		for j := 0; j < len(resultList); j++ {
+			if list[i] <= resultList[j] {
+				resultList = insertList(resultList, j, list[i])
+				break
 			}
-
+			if j == len(resultList)-1 && list[i] > resultList[j] {
+				resultList = insertList(resultList, j+1, list[i])
+				break
+			}
 		}
+		i++
 	}
+	return resultList
+}
+func insertList(list []int, i int, x int) []int {
+	returnList := []int{}
+	n := 0
+	if i == len(list) {
+		returnList = append(list, x)
+		return returnList
+	}
+	for n < len(list) {
+		if n < i {
+			returnList = append(returnList, list[n])
+		} else if n == i {
+			returnList = append(returnList, x)
+			returnList = append(returnList, list[n])
+		} else {
+			returnList = append(returnList, list[n])
+		}
+		n++
+	}
+	return returnList
 }

87b898ca5571517bd45d715cb14510638e253065
xiangdongdong <xiangdongdong@chelun.com>
2018-10-02 11:55:18 +0800

    1.插入算法

--- insertSort/insertSort.go
+++ insertSort/insertSort.go
@@ -4,7 +4,7 @@ import "fmt"
 import "../tool"
 
 func InsertSort() {
-	arrList := []int{1, 2, 3, 4, 5, 6, 8, 9, 343, 3}
+	arrList := []int{1, 2, 8, 11, 3, 6, 8, 9, 343, 3}
 	standardInsertSort(arrList)
 	fmt.Println("InsertSort:")
 	tool.PrintSlice(arrList)
@@ -12,11 +12,16 @@ func InsertSort() {
 
 func standardInsertSort(list []int) {
 	length := len(list)
-	for i := 0; i < length; i++ {
+	i := 0
+	for i < length {
+		tmp := list[i]
 		for j := i + 1; j < length; j++ {
-			if list[i] <= list[j] {
+			if tmp <= list[j] {
 				tool.SliceSwap(list, i, j)
+				tool.SliceSwap(list, i+1, j)
+				i++
 			}
+
 		}
 	}
 }

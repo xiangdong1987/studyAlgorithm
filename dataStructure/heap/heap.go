860572ce1c76a74b5030ed9051189f927eef0484
xiangdongdong <xiangdongdong@chelun.com>
2018-12-03 13:48:29 +0800

    1.完成堆的实现

--- dataStructure/heap/heap.go
+++ dataStructure/heap/heap.go
@@ -9,95 +9,127 @@ type Node struct {
 	Key   string
 }
 
-func CreateHeap() {
+type Heap struct {
+	list   []*Node
+	length int
+}
 
+//创建堆
+func CreateHeap() {
 	arrList := []int{1, 2, 11, 3, 7, 8, 4, 5}
-	var myHeap []*Node
-	myHeap = append(myHeap, &Node{})
+	var myHeap Heap
+	myHeap.list = append(myHeap.list, &Node{})
 	for _, value := range arrList {
 		tmp := Node{}
 		tmp.Value = value
-		myHeap = InsertHeap(myHeap, &tmp)
+		myHeap.InsertHeap(&tmp)
 	}
-	heapShow(myHeap)
-	myHeap = SortHeap(myHeap)
-	heapShow(myHeap)
+	for {
+		node := myHeap.GetTopHeap()
+		fmt.Println(node)
+	}
+	myHeap.SortHeap(myHeap.list)
+	heapShow(myHeap.list)
 }
 
-func InsertHeap(heaps []*Node, one *Node) []*Node {
-	heaps = append(heaps, one)
-	length := len(heaps)
-	maxPos := length - 1
-	//heapShow(heaps)
-	heaps = AdjustHeap(heaps, length, maxPos)
-	//heapShow(heaps)
-	return heaps
+//插入堆
+func (h *Heap) InsertHeap(one *Node) {
+	h.list = append(h.list, one)
+	length := len(h.list)
+	h.length = length - 1
+	h.AdjustHeap(h.length)
 }
 
-func SortHeap(heaps []*Node) []*Node {
+//堆排序
+func (h *Heap) SortHeap(heaps []*Node) {
 	length := len(heaps)
 	length = length - 1
 	if length == 1 {
-		return heaps
+		return
 	}
 	if length == 2 {
-		heaps = AdjustHeap(heaps, length-1, length)
+		h.AdjustHeap(length - 1)
 	}
 	for length > 0 {
-		SliceNodeSwap(heaps, 1, length)
+		h.SliceNodeSwap(1, length)
 		length--
-		fmt.Println(length)
-		Heapfiy(heaps, length, 1)
-		heapShow(heaps)
+		h.Heapfiy(length, 1)
 	}
-	return heaps
+	//反序
+	minPos := 1
+	maxPos := h.length
+	for minPos < maxPos {
+		h.SliceNodeSwap(minPos, maxPos)
+		minPos++
+		maxPos--
+	}
 }
 
-func AdjustHeap(heaps []*Node, length int, maxPos int) []*Node {
-	if length == 1 {
-		return heaps
+//自下而上调整
+func (h *Heap) AdjustHeap(length int) {
+	if length < 1 {
+		return
 	}
-	if maxPos == 2 {
-		if heaps[maxPos].Value > heaps[maxPos-1].Value {
-			SliceNodeSwap(heaps, maxPos, maxPos-1)
+	if length == 2 {
+		if h.list[length].Value > h.list[length-1].Value {
+			h.SliceNodeSwap(length, length-1)
 		}
-		return heaps
+		return
 	}
-	//SliceNodeSwap(heaps, 1, maxPos)
-	for length/2 > 0 && heaps[maxPos].Value > heaps[length/2].Value {
-		SliceNodeSwap(heaps, maxPos, length/2)
-		length = length / 2
-		maxPos = length
+	i := length
+	for i/2 > 0 && h.list[i].Value > h.list[i/2].Value {
+		h.SliceNodeSwap(i, i/2)
+		i = i / 2
 	}
-	//heapShow(heaps)
-	return heaps
+	return
 }
 
+//输出heap
 func heapShow(heaps []*Node) {
 	for one, value := range heaps {
 		fmt.Println(one, value)
 	}
 }
 
-func SliceNodeSwap(list []*Node, i int, j int) {
-	x := list[i]
-	list[i] = list[j]
-	list[j] = x
+//node slice交换
+func (h *Heap) SliceNodeSwap(i int, j int) {
+	x := h.list[i]
+	h.list[i] = h.list[j]
+	h.list[j] = x
 }
 
-func Heapfiy(heaps []*Node, length int, pos int) {
+//自上向下堆化
+func (h *Heap) Heapfiy(length int, pos int) {
 	for {
 		maxPos := pos
-		if pos*2 < length && heaps[pos].Value < heaps[pos*2].Value {
+		if pos*2 < length && h.list[pos].Value < h.list[pos*2].Value {
 			maxPos = pos * 2
 		}
-		if pos*2+1 < length && heaps[maxPos].Value < heaps[pos*2+1].Value {
+		if pos*2+1 < length && h.list[maxPos].Value < h.list[pos*2+1].Value {
 			maxPos = pos*2 + 1
 		}
 		if maxPos == pos {
 			break
 		}
-		SliceNodeSwap(heaps, pos, maxPos)
+		h.SliceNodeSwap(pos, maxPos)
 		pos = maxPos
 	}
+}
+
+//获取堆顶
+func (h *Heap) GetTopHeap() *Node {
+	if h.length == 0 {
+		panic("Heap is empty")
+	}
+	top := h.list[1]
+	//堆顶和堆底交换
+	h.SliceNodeSwap(1, len(h.list)-1)
+	length := len(h.list) - 2
+	fmt.Println(length)
+	h.Heapfiy(length, 1)
+	heapShow(h.list)
+	h.list = append(h.list[:length+1], h.list[length+2:]...)
+	h.length--
+	return top
+
 }

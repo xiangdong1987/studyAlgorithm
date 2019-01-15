cff7905adef6d076e903b5e168500fa4354436dd
xiangdongdong <xiangdongdong@chelun.com>
2019-01-15 09:55:42 +0800

    1.动态规划

--- algorithm/baseMind/dynOneZero.go
+++ algorithm/baseMind/dynOneZero.go
@@ -3,10 +3,14 @@ package baseMind
 import "fmt"
 
 func TestDynOneZero() {
-	max := dynOneZero()
-	fmt.Println(max)
+	max := knapsack()
+	fmt.Println("max:", max)
+	max = knapsack1()
+	fmt.Println("max1:", max)
+	max = knapsack2()
+	fmt.Println("max2:", max)
 }
-func dynOneZero() int {
+func knapsack() int {
 	var itemNum = 5
 	var bagWeight = 10
 	var items = []int{4, 5, 2, 7, 1}
@@ -32,12 +36,70 @@ func dynOneZero() int {
 		}
 		statMap[i] = tmpMap
 	}
-	fmt.Println(statMap)
+	//fmt.Println(statMap)
 	for m := bagWeight; m > 0; m-- {
 		if statMap[itemNum-1][m] {
 			return m
 		}
 	}
+	return 0
+}
 
+func knapsack1() int {
+	var itemNum = 5
+	var bagWeight = 10
+	var items = []int{3, 5, 2, 7, 1}
+	statMap := make(map[int]bool, bagWeight)
+	statMap[0] = true
+	for i := 1; i < itemNum; i++ {
+		for j := bagWeight - items[i]; j >= 0; j-- {
+			if statMap[j] == true {
+				statMap[j+items[i]] = true
+			}
+		}
+	}
+	for n := bagWeight; n >= 0; n-- {
+		if statMap[n] {
+			return n
+		}
+	}
+	return 0
+}
+
+func knapsack2() int {
+	var itemNum = 5
+	var bagWeight = 10
+	var items = []int{3, 5, 2, 7, 1}
+	var values = []int{10, 2, 15, 20, 13}
+	statMap := map[int]map[int]int{}
+	var tmpMap map[int]int
+	map0 := make(map[int]int, bagWeight)
+	map0[0] = values[0]
+	statMap[0] = map0
+	for i := 1; i < itemNum; i++ {
+		tmpMap = make(map[int]int, bagWeight)
+		//不选择第i个物品
+		for j := 0; j < bagWeight; j++ {
+			if statMap[i-1][j] > 0 {
+				tmpMap[j] = statMap[i-1][j]
+			}
+		}
+		//选择第i个物品
+		for j := 0; j < bagWeight-items[i]; j++ {
+			if statMap[i-1][j] > 0 {
+				v := statMap[i-1][j] + values[i]
+				if v > statMap[i-1][j+items[i]] {
+					tmpMap[j+items[i]] = v
+				}
+			}
+		}
+		statMap[i] = tmpMap
+	}
+	fmt.Println(statMap)
+	for n := bagWeight; n >= 0; n-- {
+		if statMap[itemNum-1][n] > 0 {
+			return statMap[itemNum-1][n]
+		}
+	}
 	return 0
 }

a31b32f25a59e528a7738a150cf1effea0d75886
xiangdongdong <xiangdongdong@chelun.com>
2020-02-12 12:49:17 +0800

    1.链表归并排序

--- problem/sortList_test.go
+++ problem/sortList_test.go
@@ -1,33 +1,35 @@
 package problem
 
 func sortList(head *ListNode) *ListNode {
-	dummy := &ListNode{}
-	dummy.Next = head
-	quickSort(dummy.Next, nil)
-	return dummy.Next
-}
-
-func quickSort(head *ListNode, end *ListNode) {
-	if head.Next == nil || head.Next == end {
-		return
+	if head == nil || head.Next == nil {
+		return head
 	}
-	dummy := &ListNode{}
-	dummy.Next = head
-	min := head
-	preNode := dummy
-	current := head
-	for current != nil {
-		if current.Val < min.Val {
-			//交换中间值和小于中间值的值
-			preNode.Next = current
-			current = min
-			min.Next = current.Next
+	//寻找中点
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	var mid *ListNode
+	mid, slow.Next = slow.Next, nil
+	//切割
+	left, right := sortList(head), sortList(mid)
+	//合并
+	result := &ListNode{}
+	current := result
+	for left != nil && right != nil {
+		if left.Val > right.Val {
+			current.Next, right = right, right.Next
+		} else {
+			current.Next, left = left, left.Next
 		}
 		current = current.Next
-		if current == end {
-			break
-		}
 	}
-	quickSort(min, nil)
-	quickSort(dummy.Next, min)
+	//将剩下的链接
+	if left != nil {
+		current.Next = left
+	} else {
+		current.Next = right
+	}
+	return result.Next
 }
